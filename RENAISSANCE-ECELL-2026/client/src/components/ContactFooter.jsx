a9216a47edd595b0c5bee253fc7d477aa2471791
Naman Kumar <krnaman.2007@gmail.com>
2026-09-19 11:34:08 +0530

    feat: add ContactFooter component and Events page

--- RENAISSANCE-ECELL-2026/client/src/components/ContactFooter.jsx
+++ RENAISSANCE-ECELL-2026/client/src/components/ContactFooter.jsx
@@ -77,7 +77,7 @@ export default function ContactFooter() {
             </a>
 
             <a
-              href="https://www.facebook.com/ecellmnnit"
+              href="https://www.facebook.com/mnnitecell/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Facebook"
